776c60a4d2b2817dddc2f64f8b96c37243ea7a43
liuweiqiu_glb <liuweiqiu_glb@126.com>
2019-03-21 17:20:32 +0800

    第七次提交

--- main.cpp
+++ main.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <QDebug>
+#include <QDebug>
+#include <QPushButton>
+
+#include <QTabWidget> //第五次修改
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    //MainWindow w;
-    //w.show();
+    MainWindow w;
+    w.show();
 
     return a.exec();
 }
